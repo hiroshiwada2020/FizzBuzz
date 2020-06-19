231a596c00f3234fefb0ee323fae3df2be9a3a7b
wada <hiroshi.wada@gmail.com>
2020-06-19 23:33:55 +0900

    15の倍数まで完了

--- fizbuz.py
+++ fizbuz.py
@@ -1,11 +1,13 @@
 def fizzbuzz_convert(number):
+    if number % 15 == 0:
+        return "FizzBuzz"
     if number % 3 == 0:
         return "Fizz"
     if number % 5 == 0:
         return "Buzz"
-    if number % 15 == 0:
-        return "FizzBuzz"
+
 
 
 assert fizzbuzz_convert(3) == "Fizz"
 assert fizzbuzz_convert(5) == "Buzz"
+assert fizzbuzz_convert(15) == "FizzBuzz"
